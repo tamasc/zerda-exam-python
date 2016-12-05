616d5ac9cd05fb4db1d21a2b37a2d9cb2c487f8b
tamasc <tamas.czerjak@gmail.com>
2016-12-05 09:35:35 +0100

    finish first task

--- first.py
+++ first.py
@@ -2,3 +2,6 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+def second_elements(input_list):
+    return input_list[1:len(input_list):2]
